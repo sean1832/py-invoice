762f3bd20aae668f1cc6065ea9cd30f0390f1867
Zeke Zhang <sean1832725142@gmail.com>
2024-01-20 16:43:12 +1100

    fix: circular import

--- invoice/core/config.py
+++ invoice/core/config.py
@@ -1,7 +1,4 @@
 from . import config_manager
 
-METADATA_PATH = 'invoice/metadata.json'
-CONFIG_PATH = 'invoice/data/config.json'
-
 path_info = config_manager.PathManager()
 project_meta = config_manager.MetadataManager()
--- invoice/core/config_manager.py
+++ invoice/core/config_manager.py
@@ -2,8 +2,8 @@ import os
 
 from invoice.core import file_io
 
-from .config import CONFIG_PATH, METADATA_PATH
-
+METADATA_PATH = 'invoice/metadata.json'
+CONFIG_PATH = 'invoice/data/config.json'
 
 class MetadataManager:
     def __init__(self):
